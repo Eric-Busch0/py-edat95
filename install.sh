d73b2ff9eda3381382fdb9bb5c1038f437144cf2
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2024-09-19 18:54:02 -0500

    fixed typo for windows and added logic to check for macos

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+
 set -e
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+brew install pipx
+pipx install --upgrade build 
+python3 -m build
+pipx install dist/*.whl --force-reinstall
+set +e
+else
 pip3 install --upgrade build 
 python3 -m build
 pip3 install dist/*.whl --force-reinstall
+fi
+
 python3 -c "import edat95"
 set +e
