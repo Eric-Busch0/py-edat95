f797b6c31c493aa603ca737c7ffb58e0aaee7095
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2024-09-19 19:04:43 -0500

    force break sys packages

--- install.sh
+++ install.sh
@@ -3,14 +3,10 @@
 set -e
 if [[ $OSTYPE == 'darwin'* ]]; then
 
-set +e
-brew install pipx
-pipx install  build 
-set -e
-
+pip3 install --upgrade build --break-system-packages
 python3 -m build
-pipx reintstall dist/*.whl
-set +e
+pip3 install dist/*.whl --force-reinstall --break-system-packages
+
 else
 pip3 install --upgrade build 
 python3 -m build
